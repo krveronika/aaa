3eefe937f089bc0d2e848a845e599d17cdc9ee71
ruakgvn <veronika.kruglikova@raiffeisen.ru>
2024-11-12 22:30:29 +0300

    [hw3] drop import, add user-define class

--- vectorizers.py
+++ vectorizers.py
@@ -1,49 +1,54 @@
-import scipy.sparse as sparse
+class SparceMatrix:
+    def __init__(self):
+        """ключ - tuple(irow, icol), значение - число повторений"""
+        self.data = dict()
+
+    def update(self, ir, ic):
+        if (ir, ic) in self.data:
+            self.data[(ir, ic)] += 1
+        else:
+            self.data[(ir, ic)] = 1
+
+    def to_array(self) -> list[list[str]]:
+        """Преобразует разреженную матрицу в плотную"""
+        max_row = max(row for row, _ in self.data.keys()) + 1
+        max_col = max(col for _, col in self.data.keys()) + 1
+        dense_matrix = [[0] * max_col for _ in range(max_row)]
+
+        for (row, col), value in self.data.items():
+            dense_matrix[row][col] = value
+        return dense_matrix
 
 
 class CountVectorizer:
-    def __init__(self, splitter=" ", is_lower=True):
+    def __init__(self, splitter: str = " ", is_lower: bool = True):
         self.splitter = splitter
         self.is_lower = is_lower
+        self._vocab: dict = dict()
+        self._sparce_matrix = SparceMatrix()
 
-    def _create_vocab(self, X):
-        """
-        Создание отсортированного по ключу словаря. Ключ - уникальное слово из X
-        """
-        set_words = set()
-        for text in X:
-            text = text.lower() if self.is_lower else text
-            set_words.update(text.split(self.splitter))
-        sorted_words = sorted(list(set_words))
-        self._vocab: dict = {w: i for i, w in enumerate(sorted_words)}
-
-    def fit_transform(self, X):
-        self._create_vocab(X)
-        rows = []
-        cols = []
-        count_word = []
-
+    def fit_transform(self, X: list[str]) -> SparceMatrix:
+        ic = -1
         for ir, text in enumerate(X):
             text = text.lower() if self.is_lower else text
-            set_words = set(text.split(self.splitter))
-            rows = rows + [ir] * len(set_words)
-            for word in set(text.split(self.splitter)):
-                cols.append(self._vocab[word])
-                count_word.append(text.count(word))
-
-        self._sparce_matrix = sparse.coo_matrix((count_word, (rows, cols)))
+            words = text.split(self.splitter)
+            for word in words:
+                if word not in self._vocab:
+                    ic += 1
+                    self._vocab[word] = ic
+                self._sparce_matrix.update(ir, self._vocab[word])
         return self._sparce_matrix
 
-    def get_feature_names(self):
-        return self._vocab.keys()
+    def get_feature_names(self) -> list:
+        return list(self._vocab.keys())
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     corpus = [
         "Crock Pot Pasta Never boil pasta again",
         "Pasta Pomodoro Fresh ingredients Parmesan to taste",
     ]
 
     count_vectorizer = CountVectorizer()
-    print(count_vectorizer.fit_transform(corpus).toarray())
+    print(count_vectorizer.fit_transform(corpus).to_array())
     print(count_vectorizer.get_feature_names())
